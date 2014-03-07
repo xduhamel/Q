af67b49ed17c5c5eb467988b783610d4d6f5ba23
Matt Duhamel <mduhamel@stanford.edu>
2014-03-06 17:18:13 -0800

    improved app visibility

--- gae/_q.py
+++ gae/_q.py
@@ -8,9 +8,10 @@ import jinja2
 import webapp2
 import json
 import time
+import cgi
 
 from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import wsgiref.handlers
 import logging
@@ -18,6 +19,10 @@ import logging
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.ext import db
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import images 
+
 
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -25,9 +30,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
         extensions=['jinja2.ext.autoescape'],
         autoescape=True)
-
-_STORAGE = 'gs'
-_FILE = 'file'
 
 DEFAULT_NAME = 'gucci'
 DEFAULT_TIME = 'just now'
@@ -75,6 +77,10 @@ class Question(ndb.Model):
     title = ndb.StringProperty('title')
     students = ndb.JsonProperty('students')
     removable = ndb.BooleanProperty('removable')
+    contacts = ndb.JsonProperty('contacts')
+    pic_id = ndb.StringProperty()
+    is_pic = ndb.BooleanProperty('is_pic')
+    ask_time = ndb.StringProperty('ask_time')
 
 
 class Class(ndb.Model):
@@ -94,6 +100,7 @@ class OfficeHours(ndb.Model):
     """ Models and individual office hours instance """
 
     creator = ndb.UserProperty()
+    identifier = ndb.IntegerProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     first_name = ndb.StringProperty('first_name')
     last_name = ndb.StringProperty('last_name')
@@ -102,6 +109,11 @@ class OfficeHours(ndb.Model):
     day = ndb.StringProperty('day')
     class_name = ndb.StringProperty('class_name')
     attending_count = ndb.IntegerProperty('count')
+    ended = ndb.BooleanProperty('ended')
+    active = ndb.BooleanProperty('acitve')
+    scheduled = ndb.BooleanProperty('scheduled')
+    start_date_time = ndb.DateTimeProperty('start_date_time')
+    end_date_time = ndb.DateTimeProperty('end_date_time')
 
 
 class Student(ndb.Model):
@@ -109,6 +121,7 @@ class Student(ndb.Model):
 
     user = ndb.UserProperty()
     classes = ndb.JsonProperty('class_list')
+    prof_pic = ndb.BlobProperty()
 
 
 class JoinOhPage(webapp2.RequestHandler):
@@ -155,17 +168,51 @@ class ViewSchedulePage(webapp2.RequestHandler):
         else:
             is_ta = True
 
-        oh_query = OfficeHours.query(OfficeHours.class_name == class_name) #.order(-OfficeHours.date)
+        oh_query = OfficeHours.query(OfficeHours.class_name == class_name).order(+OfficeHours.start_date_time)
         office_hours = oh_query.fetch()
+
+        if office_hours:
+            for oh in office_hours:
+                
+                # hack to account for utc time difference
+                now = datetime.now() - timedelta(hours=7)
+                
+                if oh.end_date_time < now:
+                    oh.ended = True
+                    oh.active = False
+                    oh.scheduled = False
+
+                elif oh.start_date_time < now and now < oh.end_date_time:
+                    oh.ended = False
+                    oh.active = True
+                    oh.scheduled = False
+
+                else:
+                    oh.ended = False
+                    oh.active = False 
+                    oh.scheduled = True
+        
+        if not office_hours:
+            no_office_hours = True
+        
+        else:
+            no_office_hours = False
+
+        if len(class_name) > 10:
+            nick_name = class_name[0:10] + '...'
+        else:
+            nick_name = class_name 
 
         template = JINJA_ENVIRONMENT.get_template('join_oh.html')
 
         template_values = {
+            'no_oh': no_office_hours,
+            'nick_name': nick_name,
             'class_name': class_name,
             'office_hours': office_hours,
-            'url': url,
             'url_linktext': url_linktext,
-            'is_ta': is_ta
+            'is_ta': is_ta,
+            'url': url
         }
 
         self.response.write(template.render(template_values))
@@ -195,10 +242,11 @@ class MyClassesPage(webapp2.RequestHandler):
                         my_classes = json.loads(my_classes)
 
             else:
-                new_student = Student()
-                new_student.user = current_user
-                new_student.classes = []
-                new_student.put()
+                current_student = Student()
+                current_student.user = current_user
+                current_student.classes = []
+                current_student.prof_pic = None
+                current_student.put()
 
         else:
             url = users.create_login_url(self.request.uri)
@@ -206,15 +254,19 @@ class MyClassesPage(webapp2.RequestHandler):
             html_template = 'login.html'
 
         template = JINJA_ENVIRONMENT.get_template(html_template)
-
+        
         template_values = {
             'classes': my_classes,
             'url': url,
             'url_linktext': url_linktext,
         }
 
-        self.response.write(template.render(template_values))
+        upload_url = blobstore.create_upload_url('/upload')
 
+        self.response.write(template.render(template_values))
+        # self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        # self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+        # name="submit" value="Submit"> </form></body></html>""")
 
 class JoinClassPage(webapp2.RequestHandler):
     """ Handles joining a class """
@@ -224,7 +276,6 @@ class JoinClassPage(webapp2.RequestHandler):
         all_classes = DEFAULT_CLASS
         class_query = Class.query(Class.valid_class == True).order(-Class.creation_time)
         classes = class_query.fetch()
-
         current_user = users.get_current_user()
 
         student_query = Student.query(Student.user == current_user)
@@ -237,17 +288,19 @@ class JoinClassPage(webapp2.RequestHandler):
         for class_ in classes:
             
             current_classes = current_student.classes
-            if not isinstance(current_classes, list):
+            if current_classes is None:
+                current_classes = []
+
+            elif not isinstance(current_classes, list):
                 current_classes = json.loads(current_classes)
 
-            logging.info(class_.title)
+
 
             #if class_.title not in current_classes or class_.title is not None:
             #    real_classes.append(class_)
 
             if class_.title not in current_classes:
-                logging.info('PARTAY!')
-                if class_.title is not None:
+                if len(class_.title) is not 0:
                     real_classes.append(class_)
 
         if users.get_current_user():
@@ -269,20 +322,27 @@ class JoinClassPage(webapp2.RequestHandler):
 
         self.response.write(template.render(template_values))
 
-
 class QPage(webapp2.RequestHandler):
     """ Handles the displaying of the Q. """
 
     def get(self, queue):
 
+
         class_name = queue.split('+')[0]
         link_name = queue.split('+')[1]
+        _id = int(link_name)
 
         question_name = queue
-        questions_query = Question.query(ancestor=question_key(queue)).order(-Question.tally)  # HERE ---------
+        questions_query = Question.query(ancestor=question_key(queue)).order(-Question.tally)
         questions = questions_query.fetch()
-
         current_user = users.get_current_user()
+
+        oh_query = OfficeHours.query(OfficeHours.identifier == _id)
+        oh = oh_query.fetch()
+        if oh:
+            current_office_hours = oh[0]
+            current_office_hours.attending_count += 1
+            current_office_hours.put()
 
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -298,10 +358,11 @@ class QPage(webapp2.RequestHandler):
         classes = class_query.fetch()
 
         if classes:
+
             current_class = classes[0]
             current_tas = current_class.tas
-
             current_students = current_class.students
+
             if not isinstance(current_students, list):
                 current_students = json.loads(current_students)
 
@@ -309,17 +370,21 @@ class QPage(webapp2.RequestHandler):
                 is_student = True
 
             else:
-                is_student = True
+                is_student = False
 
             if not isinstance(current_tas, list):
                 current_tas = json.loads(current_tas)
 
             if current_user.user_id() in current_tas:
-                
                 for question in questions:
                     question.removable = True
+                    if question.tally == 0:
+                        question.answered = True
+                        question.removable = True  
+                is_ta = False
 
             else:
+                is_ta = True
                 for question in questions:
                     if not question.students:
                         question.students = []
@@ -329,7 +394,7 @@ class QPage(webapp2.RequestHandler):
 
                     else:
                         question.done = False
-
+                    
                     if question.tally == 0:
                         question.answered = True
                         question.removable = True 
@@ -343,7 +408,9 @@ class QPage(webapp2.RequestHandler):
         else:
             is_student = False
 
+
         template = JINJA_ENVIRONMENT.get_template('index.html')
+
         template_values = {
                 'url': url,
                 'class_name': class_name, 
@@ -354,6 +421,8 @@ class QPage(webapp2.RequestHandler):
                 'name_tag': 'generic_tag',
                 'question_time': DEFAULT_TIME,
                 'current_user': is_student,
+                'link_name': link_name,
+                'is_student': is_ta,
         }
 
         self.response.write(template.render(template_values))
@@ -465,11 +534,17 @@ class NewClass(webapp2.RequestHandler):
 
 class NewQuestion(webapp2.RequestHandler):
 
-    def get(self, question_name):
+    def get(self, arg):
+
+        question_name = arg.split('|')[0]
+        link_name = arg.split('|')[1]
+        upload_url = blobstore.create_upload_url('/ask/%s' % question_name)
 
         template = JINJA_ENVIRONMENT.get_template('new_question.html')
         template_values = {
+            'upload_url': upload_url, 
             'question_name': question_name,
+            'link_name': link_name,
         }
 
         self.response.write(template.render(template_values))
@@ -498,29 +573,25 @@ class Map(webapp2.RequestHandler):
 
         self.response.write(template.render(template_values))
 
-class PostProblem(webapp2.RequestHandler):
 
-    def post(self, arg):
-        question_name = arg
-        question = Question(parent=question_key(question_name))
+class ViewQuestionMembers(webapp2.RequestHandler):
 
-        if users.get_current_user():
-            question.author = users.get_current_user()
+    def get(self, q_id):
 
-        question.title = self.request.get('title')
-        question.content = self.request.get('content')
-        question.q_id = self.get_question_id(question.author)
-        question.answered = False
-        question.done = True
-        question.tally = 1
-        question.students = [question.author.user_id()]
-        question.put()
+        question_query = Question.query(Question.q_id == q_id)
+        question = question_query.fetch()
 
-        query_params = {'question_name': question_name}
-        self.redirect('/q/' + question_name)
+        if question:
+            current_question = question[0]
+            contacts = current_question.contacts
 
-    def get_question_id(self, author):
-        return str(hash(author)) + str(datetime.now().microsecond)
+            template = JINJA_ENVIRONMENT.get_template('question_members.html')
+            
+            template_values = {
+                'contacts': contacts,
+            }
+
+            self.response.write(template.render(template_values))
 
 
 class CreateClass(webapp2.RequestHandler):
@@ -563,6 +634,18 @@ class CreateOfficeHours(webapp2.RequestHandler):
         office_hours.end_time = self.request.get('end_time')
         office_hours.day = self.request.get('day')
         office_hours.class_name = class_name
+        office_hours.identifier = datetime.now().microsecond
+        office_hours.attending_count = 0
+        office_hours.ended = False
+        office_hours.active = False
+        office_hours.scheduled = True
+
+        convert_start = office_hours.day + ' ' + office_hours.start_time
+        convert_end = office_hours.day + ' ' + office_hours.end_time
+
+        office_hours.start_date_time = datetime.strptime(convert_start, '%Y-%m-%d %H:%M')
+        office_hours.end_date_time = datetime.strptime(convert_end, '%Y-%m-%d %H:%M')
+
         office_hours.put()
 
         self.redirect('/schedule/' + class_name)
@@ -592,13 +675,30 @@ class JoinQuestion(webapp2.RequestHandler):
                 current_question.done = None
                 other_students.remove(joiner.user_id())
                 current_question.tally -= 1
-                current_question.students = other_students                
+                current_question.students = other_students  
+                # REMOVE EMAIL FROM CONTACTS  
+
+                if not current_question.contacts:
+                    current_question.contacts = []
+
+                elif joiner.email() in current_question.contacts:
+                    current_contacts = current_question.contacts
+                    current_contacts.remove(joiner.email())
+                    current_question.contacts = current_contacts           
 
             else:
                 other_students.append(joiner.user_id())
                 current_question.students = other_students
                 current_question.tally += 1
                 current_question.done = True
+
+                if not current_question.contacts:
+                    current_question.contacts = [joiner.email()]
+
+                else:
+                    current_contacts = current_question.contacts
+                    current_contacts.append(joiner.email())
+                    current_question.contacts = current_contacts
 
             current_question.put()
 
@@ -761,9 +861,11 @@ class RemoveClass(webapp2.RequestHandler):
                 class_ = class_query.fetch()
                 if class_:
                     current_class = class_[0]
-
-                    #if current_user.user_id() in current_class.students:
-                    #       current_class.students.
+                    if current_user.user_id() in current_class.tas:
+                        new_ta_list = current_class.tas
+                        new_ta_list.remove(current_user.user_id())
+                        current_class.tas = new_ta_list
+                        current_class.put()
 
         self.redirect('/')
 
@@ -786,6 +888,70 @@ class DeleteClass(webapp2.RequestHandler):
 
         self.redirect('/join')
 
+
+class PostProblem(blobstore_handlers.BlobstoreUploadHandler):
+
+    def post(self, arg):
+        question_name = arg
+        question = Question(parent=question_key(question_name))
+
+        if users.get_current_user():
+            question.author = users.get_current_user()
+        
+        upload_files = self.get_uploads('file')
+        if upload_files:
+            blob_info = upload_files[0]
+            if blob_info:
+                question.pic_id = str(blob_info.key())
+                question.is_pic = True
+        else:
+            question.pic_id = None
+            question.is_pic = False
+
+        question.title = self.request.get('title')
+        question.content = self.request.get('content')
+        question.q_id = self.get_question_id(question.author)
+        question.answered = False
+        question.done = True
+        question.tally = 1
+        question.students = [question.author.user_id()]
+        question.contacts = [question.author.email()]
+        question.ask_time = datetime.strftime(datetime.now() - timedelta(hours=7), "%a, %b, %d %X")
+        question.put()
+
+        query_params = {'question_name': question_name}
+        self.redirect('/q/' + question_name)
+
+    def get_question_id(self, author):
+        return str(hash(author)) + str(datetime.now().microsecond)
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+
+    def post(self):
+        upload_files = self.get_uploads('file')
+        blob_info = upload_files[0]
+        self.redirect('/blob/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        #logging.ino(blob_info)
+        self.send_blob(blob_info)
+
+
+class Blob(webapp2.RequestHandler):
+
+    def get(self, blob_key):
+
+        template = JINJA_ENVIRONMENT.get_template('blob.html')
+        template_values = {
+            'blob_key': blob_key,
+        }
+
+        self.response.write(template.render(template_values))
+
+
 app = webapp2.WSGIApplication([
     (r'/', MyClassesPage),
     (r'/schedule/([^/]+)', ViewSchedulePage),
@@ -807,4 +973,8 @@ app = webapp2.WSGIApplication([
     (r'/create_class/([^/]+)', CreateClass),
     (r'/create_oh/([^/]+)', CreateOfficeHours),
     (r'/map', Map),
+    (r'/question_members/([^/]+)', ViewQuestionMembers),
+    (r'/_ah/upload', UploadHandler),
+    (r'/q/serve/([^/]+)?', ServeHandler),
+    (r'/blob/([^/]+)', Blob)
 ], debug=True)
